3d920880151813b58e9dd0c3d6fb0852ffcf9170
chris-hamberg <chris.hamberg@programmer.net>
2019-02-27 08:26:33 +0000

    embedded RSS feed reader application

--- application/mvc/apps.py
+++ application/mvc/apps.py
@@ -1,5 +1,9 @@
 from flask import Blueprint, render_template
 from flask_login import login_required
+import sys
+
+sys.path.append('application/data/Applications')
+from application.data.Applications.reuters import Reuters
 
 applications = Blueprint('applications', __name__)
 
@@ -7,7 +11,8 @@ applications = Blueprint('applications', __name__)
 def reuters():
     return render_template('reuters.html',
             cms=applications.cms,
-            navbar=applications.navbar('reuters'))
+            navbar=applications.navbar('reuters'),
+            reuters=Reuters())
 
 @applications.route('/twitter/')
 @login_required
